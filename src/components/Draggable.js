6ad8923dcfc83ae91cbe052b32b2a4b9be402a05
Joseph Thomas Vasquez <joetv.tech@gmail.com>
2021-06-10 20:28:04 -0700

    Added state to draggable component

--- src/components/Draggable.js
+++ src/components/Draggable.js
@@ -1,6 +1,14 @@
-import React from "react";
+import React, { useState, useMemo } from "react";
+
+const mousePosition = { x: 0, y: 0 };
 
 const Draggable = ({ children }) => {
+  const [dragItem, setDragItem] = useState({
+    isDragging: false,
+    origin: mousePosition,
+    translation: mousePosition
+  });
+
   return <div>{children}</div>;
 };
 
